68378afe1117e74f050b7dbd72eb976914da2eee
Junyoung Park <junyoungpark@kaist.ac.kr>
2020-07-29 18:01:48 +0900

    fix email address

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='GraphFloris',
-    version='0.11',
+    version='0.12',
     long_description=long_description,
     long_description_content_type='text/markdown',
     description='Graph represented wind farm power simulator',
     author='Junyoung Park',
-    author_email='songew@gmail.com',
+    author_email='Junyoungpark@kaist.com',
     url='https://github.com/Junyoungpark/GraphFloris',
     download_url='https://github.com/Junyoungpark/GraphFloris',
     install_requires=['floris', 'torch', 'dgl', 'networkx', 'matplotlib'],
